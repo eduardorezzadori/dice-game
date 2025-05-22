303dcc9b2e0752db6b0430ea7bb3af7ae20ad86d
Eduardo Rezzadori <eduardo.rezzadori@inova.ind.br>
2025-05-22 08:03:28 -0300

    chore: internacionalização

--- app/src/main/java/com/example/dados/MainActivity.kt
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -3,30 +3,43 @@ package com.example.dados
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var previousResult = 0
+    private var randomValue = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.butao)
-        val ibagem = findViewById<ImageView>(R.id.imagem)
+        val button = findViewById<Button>(R.id.playButton)
+        val image = findViewById<ImageView>(R.id.diceImage)
+        val diceResult = findViewById<TextView>(R.id.diceResult)
+
+        diceResult.text = getString(R.string.dice_result, randomValue)
 
         button.setOnClickListener {
-            val value = (1..6).random()
+            randomValue = (1..6).random()
 
-            when (value) {
-                1 -> ibagem.setImageResource(R.drawable.dice1)
-                2 -> ibagem.setImageResource(R.drawable.dice2)
-                3 -> ibagem.setImageResource(R.drawable.dice3)
-                4 -> ibagem.setImageResource(R.drawable.dice4)
-                5 -> ibagem.setImageResource(R.drawable.dice5)
-                6 -> ibagem.setImageResource(R.drawable.dice6)
+            when (randomValue) {
+                1 -> image.setImageResource(R.drawable.dice1)
+                2 -> image.setImageResource(R.drawable.dice2)
+                3 -> image.setImageResource(R.drawable.dice3)
+                4 -> image.setImageResource(R.drawable.dice4)
+                5 -> image.setImageResource(R.drawable.dice5)
+                6 -> image.setImageResource(R.drawable.dice6)
             }
+            diceResult.text = getString(R.string.dice_result, randomValue)
+
+            if (previousResult == randomValue) {
+                Toast.makeText(this, getString(R.string.toast_info), Toast.LENGTH_SHORT).show()
+            }
+
+            previousResult = randomValue
         }
     }
 }
